d07efd4ffca1162ec837783042d6b04e18159487
Cami_Costa <camily.bc.veleiros@gmail.com>
2024-09-25 15:09:10 -0300

    att final resposivo

--- Perfil Luiza/js/script.js
+++ Perfil Luiza/js/script.js
@@ -1,7 +1,14 @@
+// Mudança de tema de dark para light
 const toggleTheme = document.getElementById("toggleTheme"); /* chama o elemento toggleTheme */
 const rootHtml = document.documentElement;
 
-// função para alterar o tema
+// Accordion
+
+const accordionHeaders = document.querySelectorAll(".accordion-header");  // o querySelectorAll pega todos os accordion-header do documento (por isso "seletor de tudo")
+const menuLinks = document.querySelectorAll(".menu-link"); // o mesmo nesse,o querySelectorAll seleciona todos os menu-link do documento
+
+
+// Função para Alterar o Tema
 
 function changeTheme () {
     const currentTheme = rootHtml.getAttribute("data-theme");
@@ -13,4 +20,17 @@ function changeTheme () {
 }
 
 toggleTheme.addEventListener("click", changeTheme);
-// fim da função de mudar tema
+// Fim da Função de Mudar Tema
+
+// Função Click da área Accordion(abre e fecha do curso)
+// forEach = para cada
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active"); // adiciona e remove o active do accordion-item
+    })
+})
+
+// Fim Função Click da área Accordion(abre e fecha do curso)
